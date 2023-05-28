00696ac0c79cd00a21a88854c8494d06127a0b45
theocod3s <theophilusdev@gmail.com>
2023-05-28 02:12:10 +0100

    read env variables with viper

--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/theodod3s/simplebank/api"
 	db "github.com/theodod3s/simplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simplebank?sslmode=disable"
-	serverAddress = "localhost:8080"
+	"github.com/theodod3s/simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
